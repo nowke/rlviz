f101231ddfaa8ec62e6cd261d4002a4cd7faa002
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2020-08-25 17:25:57 -0700

    Change page title

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   publicPath: process.env.NODE_ENV === "production" ? "/rlviz" : "/",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "GridWorld RL"
+    }
+  },
   pluginOptions: {
     webpackBundleAnalyzer: {
       openAnalyzer: false,
-      analyzerPort: process.env.VUE_CLI_MODERN_BUILD ? 8888 : 9999
+      analyzerPort: process.env.VUE_CLI_MODERN_BUILD ? 8723 : 9923
     }
   }
 };
